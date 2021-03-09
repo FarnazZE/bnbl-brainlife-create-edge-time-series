37c90d2d641dae60503199b4eef494e8b778ac21
FarnazZE <fzamani@bl-psy-flbetz3.attlocal.net>
2021-03-09 15:12:34 -0500

    Commit information

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import h5py
 
 
 # Choosing config file
-configFilename = "config-sample.json"
+configFilename = "config.json"
 argCount = len(sys.argv)
 if(argCount > 1):
     configFilename = sys.argv[1]
