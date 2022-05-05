7b46b6a78761b6f3e0d42040c7d35326f4d11a6c
Farnaz Zamani Esfahlani <53013445+FarnazZE@users.noreply.github.com>
2022-05-04 22:29:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -32,10 +32,31 @@ data_file = str(config['timeseries'])
 
 print("Loading time series...")
 
-ts=pd.read_csv('data_file')
-ts=ts['timeseries']
-#hf = h5py.File(data_file,'r') #load data 
-#reglabs = np.array(hf.get('regionids')).astype(np.str) #region_ids
-#ts = np.array(hf.get('timeseries')) #time series
+
+hf = h5py.File(data_file,'r') #load data 
+reglabs = np.array(hf.get('regionids')).astype(np.str) #region_ids
+ts = np.array(hf.get('timeseries')) #time series
+
+# z-scored time series
+z = stats.zscore(ts,0)
 
 
+print("Building edge time series...")
+T, N= ts.shape
+u,v = np.where(np.triu(np.ones(N),1))           # get edges
+# element-wise prroduct of time series
+ets = (z[:,u]*z[:,v])
+edgeids = [edge for edge in zip(u,v)]
+
+hf.close()                            
+# np.savetxt('outputDirectory/edge_timeseries.csv',ets.transpose(),delimiter=',') 
+
+print("Saving hdf5 file...")
+with h5py.File(Path(outputDirectory) / "timeseries.hdf5", "w")as h5f:
+    h5f.create_dataset('timeseries',
+                        data=ets,
+                        compression="gzip")
+    h5f.create_dataset('regionids',
+                        data=np.array(edgeids))
+
+# When registering the app add tag to specity this is an edge-based time series
