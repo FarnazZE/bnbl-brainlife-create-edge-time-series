b2b5a5ba1db4b9b510b223bb784fa6cf661e3274
Farnaz Zamani Esfahlani <53013445+FarnazZE@users.noreply.github.com>
2022-10-18 21:44:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ edgeids = {"edgeid"+str(e):edge for e,edge in enumerate(zip(columns[u],columns[v
 
 
 
-
-np.savetxt('output/timeseries.tsv.gz',ets,delimiter=',')
+ets_pd = pd.DataFrame(ets, columns=np.arange(np.size(ets,1)))
+np.savetxt('output/timeseries.tsv.gz',ets_pd,delimiter=',')
 
 with open('output/label.json', 'w') as outfile:
      outfile.write(json.dumps(edgeids))
